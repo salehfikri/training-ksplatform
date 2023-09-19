ee1f2d56f88612154ac406de8df9ada4cad958a4
salehfikri <salehfikri@gmail.com>
2023-09-19 10:38:26 +0800

    init

---
+++ users.php
@@ -0,0 +1,11 @@
+<?php
+
+$users = [
+    [
+        'id' => 1,
+        'name' => 'Mrs. Vilma Hara',
+        'email' => 'hhills@example.net',
+        'created_at' => '2023-07-20 07:29:42',
+        'updated_at' => '2023-07-20 07:29:42'
+    ],
+];
---
+++ users.sql
@@ -0,0 +1,13 @@
+-- Tasks
+
+-- Convert the following SQL statement to a users PHP array in users.php file, with the following criteria;
+    -- Change domain name 'example' to 'gmail'
+    -- Add auto increment 'id' for each rows
+    -- Remove 'password'
+
+INSERT INTO `users` (`name`, `email`, `password`, `created_at`, `updated_at`) VALUES
+('Mrs. Vilma Hara', 'hhills@example.net', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '2023-07-20 07:29:42', '2023-07-20 07:29:42'),
+('Dr. Genesis Grady', 'manley80@example.net', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '2023-07-20 07:29:42', '2023-07-20 07:29:42'),
+('Hal Halvorson', 'sallie76@example.org', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '2023-07-20 07:29:42', '2023-07-20 07:29:42'),
+('Mrs. Sydnee Jenkins', 'lexie.buckridge@example.net', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '2023-07-20 07:29:42', '2023-07-20 07:29:42'),
+('Mabel West', 'hattie.carter@example.net', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '2023-07-20 07:29:43', '2023-07-20 07:29:43');
